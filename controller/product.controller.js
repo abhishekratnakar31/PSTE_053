abaa664ebd3d2db1b22539f27b31ff101854cee4
abhishekratnakar31 <ratnakarabhishek36@gmail.com>
2025-05-05 12:29:18 +0530

    not working

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -26,45 +26,45 @@ let getallproducts=((req,res)=>{
 
 
 
-// let addproduct=((req,res)=>{
-//       try {
-//         let body = req.body;
-//         let error= validateproduct(body);
-//         if(error){
-//             return res.json({ success: true, message: "product is required" }).status(400);
-//         }
+let addproduct=((req,res)=>{
+      try {
+        let body = req.body;
+        let error= validateproduct(body);
+        if(error){
+            return res.json({ success: true, message: "product is required" }).status(400);
+        }
 
-//         products.push(body);
-//         res.status(200).json({ success: true, meesage: "Products added " });
-//       } catch (err) {
-//         res.status(500).json({ success: false, message: "Internal server error" });
-//       }
+        products.push(body);
+        res.status(200).json({ success: true, meesage: "Products added " });
+      } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error" });
+      }
   
-// });
+});
 
-// let deleteproduct=((req,res)=>{
-//     let productId=req.params.id;
-//     let product=products.find((p)=>{
-//         return p.id==productId
-//     })
-//     if(!product){
-//         return res.status(404).json({success:false,message:"product not found"})
-//     }
-//     products=products.filter((p)=>{
-//         return p.id!=productId
-//     })
-//     res.status(200).json({success:true,message:"Product deleted",data:product})
-// })
+let deleteproduct=((req,res)=>{
+    let productId=req.params.id;
+    let product=products.find((p)=>{
+        return p.id==productId
+    })
+    if(!product){
+        return res.status(404).json({success:false,message:"product not found"})
+    }
+    products=products.filter((p)=>{
+        return p.id!=productId
+    })
+    res.status(200).json({success:true,message:"Product deleted",data:product})
+})
 
 
-// let validateproduct= (product)=>{
-//     if(!product) {
-//         throw new Error("Please pass a valid product")
-//     }
-//     if(!product.name || product.category || product.price || product.discount){
-//         return new Error("Product is req")
-//     }
-// }
+let validateproduct= (product)=>{
+    if(!product) {
+        throw new Error("Please pass a valid product")
+    }
+    if(!product.name || product.category || product.price || product.discount){
+        return new Error("Product is req")
+    }
+}
 
 
 module.exports={getallproducts, addproduct, deleteproduct, validateproduct};
